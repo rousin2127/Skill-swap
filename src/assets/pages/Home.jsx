0352f7b88657e8549a98ec85c402e71c2be78853
rousin2127 <rousinmd@gmail.com>
2025-10-23 16:14:09 +0600

    create project boilerplate and basic routing

---
+++ src/assets/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='text-red-500'>
+                This is Home page 
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './assets/layout/Root.jsx';
+import Home from './assets/pages/Home.jsx';
+
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    Component:Root,
+    children: [
+      {
+        index: true,
+        Component: Home
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
