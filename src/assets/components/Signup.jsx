aa400d1f0f87ef7e17dbc7bc90a7e85a0140b77c
rousin2127 <rousinmd@gmail.com>
2025-10-25 15:15:10 +0600

    implement signup with password validation

--- src/assets/components/Signup.jsx
+++ src/assets/components/Signup.jsx
@@ -1,45 +1,70 @@
 // import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import { Link } from 'react-router';
 
 import { updateProfile } from 'firebase/auth';
 import { AuthContext } from '../../context/AuthContext';
+import { p } from 'framer-motion/client';
 // import { auth } from '../firebase/firebase.init';
 
 const Signup = () => {
-    const {createUser} = use(AuthContext)
+    const { createUser } = use(AuthContext)
+
+    const [success, setSuccess] = useState(false)
+    const [error, setError] = useState('');
 
 
-    const handleSignUp =(event)=>{
+    const handleSignUp = (event) => {
         event.preventDefault()
         const email = event.target.email.value
         const password = event.target.password.value
-        const name= event.target.name.value
-        const photo= event.target.photo.value
+        const name = event.target.name.value
+        const photo = event.target.photo.value
 
-        console.log(email,password,name, photo)
+        console.log(email, password, name, photo)
 
 
-        createUser(email,password)
-        .then(result=>{
-            console.log(result.user);
-            // event.target.reset()
-            const profile= {
-                displayName: name,
-                photoURL: photo
-            }
-            updateProfile(result.user, profile)
-            .then( ()=>{
+        const passwordPattern = /^.{6,}$/;
+        const casePattern = /^(?=.*[a-z])(?=.*[A-Z]).+$/;
+        if (!passwordPattern.test(password)) {
+            // console.log('dont match ');
+            setError('Please give me at least 6 character or Longer.')
+            return;
+        }
+        else if (!casePattern.test(password)) {
+            setError('Password must have at least one upper and one lower character.')
+            return;
+        }
+
+
+        // reset error ans Success
+        setError('');
+        setSuccess(false)
+
+
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user);
+                // event.target.reset()
+                const profile = {
+                    displayName: name,
+                    photoURL: photo
+                }
+                updateProfile(result.user, profile)
+                    .then(() => {
+
+                    })
+                    .catch(error => {
+
+                    })
+                setSuccess(true)
 
             })
-            .catch(error =>{
+            .catch(error => {
+                console.log(error);
+                setError(error.message)
 
             })
-        })
-        .catch(error=>{
-            console.log(error);
-            
-        })
     }
 
     // const handleSignUp= (event)=>{
@@ -64,7 +89,7 @@ const Signup = () => {
 
     return (
 
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl m-auto mt-[20vh]">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl m-auto my-[10vh]">
             <div className="card-body">
                 <h1 className="text-5xl font-bold">SignUp now!</h1>
                 <form onSubmit={handleSignUp}>
@@ -84,6 +109,12 @@ const Signup = () => {
                         {/* <div><a className="link link-hover">Forgot password?</a></div> */}
                         <button className="btn btn-neutral mt-4">SignUp</button>
                     </fieldset>
+                    {
+                        success && <p className='text-green-500'>Account Created Successfully</p>
+                    }
+                    {
+                        error && <p className='text-red-500'>{error}</p>
+                    }
                     <p>Already Have an account? <Link className='text-blue-600' to={'/login'}>Login</Link> </p>
                 </form>
             </div>
